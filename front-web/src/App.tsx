6ae4789bf20edc8c4a34913b6fcfaf5ecd0f641b
Salão do Reino <gigliozzi@gmail.com>
2021-01-08 19:19:15 -0300

    Add Footer

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,4 +1,5 @@
 import './App.css';
+import Footer from './Footer';
 import Home from './Home';
 import Navbar from './Navbar';
 
@@ -6,7 +7,7 @@ function App() {
   return (
     <div>
       <Navbar />
-      <Home />
+      <Home />      
     </div>
   );
 }
