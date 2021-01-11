95b0f61101f9298285f102395f4a5279aa112150
Salão do Reino <gigliozzi@gmail.com>
2021-01-10 22:24:05 -0300

    Add Search and Map

--- front-web/src/Orders/index.tsx
+++ front-web/src/Orders/index.tsx
@@ -2,11 +2,13 @@ import { useEffect, useState } from 'react'
 import './styles.css'
 import StepsHeader from './StepsHeader'
 import ProductList from './ProductList'
-import { Product } from './types'
+import { OrderLocationdata, Product } from './types'
 import { fetchProducts } from '../api';
+import OrderLocation from './OrderLocation'
 
 function Orders() {
     const [products, setProducts] = useState<Product[]>([]);
+    const [orderLocation, setOrderLocation] = useState<OrderLocationdata>();
     
     useEffect(() => {
         fetchProducts()
@@ -18,6 +20,7 @@ function Orders() {
         <div className="orders-container">
             <StepsHeader />
             <ProductList products={products} />
+            <OrderLocation onChangeLocation={location => setOrderLocation(location)}/>
         </div>
         
     )
